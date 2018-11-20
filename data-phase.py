e40fc56116355daa46a404e6858e61ed386cf088
Adam Wilkinson <adam.wilkinson07@gmail.com>
2018-11-20 17:57:21 -0500

    Create data-phase.py

---
+++ data-phase.py
@@ -0,0 +1,7 @@
+# This file contains all of the data preprocessing functions required analyzing a data.
+# Functions include querying data, cleaning data, normalizing data and visualizing data.
+
+def dataquery():
+
+return
+
